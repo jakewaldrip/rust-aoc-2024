97302576745848acc3499d97326fc6be3d6c513b
Jacob Waldrip <jakeywaldrip@gmail.com>
2025-10-14 16:33:25 -0500

    Added day 4 example test

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -11,4 +11,12 @@ pub fn solve(input: &str) -> SolutionPair {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_example_input_p1() {
+        let input = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX";
+        let (p1, _) = solve(input);
+        let p1_result = format!("{p1}");
+        assert_eq!(p1_result, "18");
+    }
 }
