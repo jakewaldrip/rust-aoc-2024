dd662a0a941efd7f124c26f277414baa5bc01ace
jacob-waldrip <jacob.waldrip@cityblock.com>
2025-10-30 21:56:58 -0400

    Wrote a test

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -3,6 +3,7 @@ use crate::{Solution, SolutionPair};
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve(input: &str) -> SolutionPair {
+    println!("Input: \n{input}");
     let sol1 = 0;
     let sol2 = 0;
     (Solution::from(sol1), Solution::from(sol2))
@@ -11,4 +12,12 @@ pub fn solve(input: &str) -> SolutionPair {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_example_input_day5_p1() {
+        let input = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47";
+        let (p1, _) = solve(input);
+        let p1_result = format!("{p1}");
+        assert_eq!(p1_result, "143");
+    }
 }
